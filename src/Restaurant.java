2301204d86249e6d86377e6c32d7231b6913c060
petros05 <abrahapetros0@gmail.com>
2026-01-28 14:16:28 -0700

    init

---
+++ src/Restaurant.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Restaurant {
+    // Properties
+    private int waitTime;
+    private double rating;
+    private double avrPrice;
+    private double distance;
+    private String hoursOpen;
+
+    // Constructor
+    public Restaurant(int waitTime, double rating, double avrPrice, double distance, String hoursOpen) {
+        this.waitTime = waitTime;
+        this.rating = rating;
+        this.avrPrice = avrPrice;
+        this.distance = distance;
+        this.hoursOpen = hoursOpen;
+    }
+}
+
