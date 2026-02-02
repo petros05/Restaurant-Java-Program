66fdd954323182d20408866274bc8c87cba16672
petros05 <abrahapetros0@gmail.com>
2026-02-02 11:20:59 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Restaurant mcDonald = new Restaurant(30, 4.5, 14.0, 5.6, "8am - 9mp");
+        Restaurant mcDonald = new Restaurant(30, 4.5, 14.0, 5.6, "8am - 9pm");
         System.out.println(mcDonald);
     }
 }
